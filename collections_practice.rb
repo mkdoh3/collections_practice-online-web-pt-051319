b509e7c6f2a99e6a9900889fdd46c887ca3a7766
Michael Doherty <michael.doherty@flatironschool.com>
2019-05-21 01:55:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,66 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+# def sort_array_desc(array)
+#   array.sort.reverse
+# end
+
+def sort_array_desc(array)
+  array.sort{|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+# def proper_kesha_maker(arr)
+#   arr.map do |string|
+#     chars = string.split('')
+#     chars.each_with_index.map do |char, index|
+#       char = '$' if char == 's' && chars[index + 1] == 'h'
+#       char
+#     end
+#   end
+# end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+
+def add_s(array)
+  array.map.with_index do |word, index|
+    if index != 1 
+      word << 's'
+  end
+      word
+  end
+end
+
+
+
+
 
